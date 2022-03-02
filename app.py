f5aeef8ecf5f2e9d4befd2f952a0de113433f436
Gil Elbaz <77351401+GilElbaz-Code@users.noreply.github.com>
2022-03-02 15:14:26 +0200

    need to connect ui to api

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
+from forms import DeleteForm
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+Bootstrap(app)
 
 
 @app.route('/')
 def home():
-    return render_template('Buttons.html')
+    return render_template('layout.html')
 
 
 @app.route('/add')
@@ -13,5 +17,15 @@ def add():
     return "Add"
 
 
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+    form = DeleteForm(request.form)
+    if request.method == 'POST' and form.validate():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+    return render_template('delete_member.html', form=form)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class DeleteForm(Form):
+    first_name = StringField('First Name',
+                             validators=[DataRequired(), Length(min=2, max=30)])
+    last_name = StringField('Last Name',
+                            validators=[DataRequired(), Length(min=2, max=30)])
+    submit = SubmitField('Delete Member')
